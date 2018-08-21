9ad844e8cc1288307db18da55ebf3ff16553bb9c
ilyana19 <aries_2152@hotmail.com>
2018-08-21 14:55:58 -0400

    created some restaurant seed data and basic controller for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+
+restaurants = [
+  {name: "The Green Dragon", neighbourhood: 'Toronto', address: '123 Fake Street East', price_range: '15-20', menu: 'They serve dragons.', summary: "The place where the coolest vertically challenged citizens of Middle-Earth get loud and tell tall tales", owner_id: nil},
+  {name: "Cantina at Mos Eisley", neighbourhood: 'North York', address: '2345 Mos Eisley', price_range: '35-60', menu: 'Any food.', summary: "If you want to prove you can hang with scum and villainy's greatest hits, this is your joint", owner_id: nil},
+  {name: "The Hog's Head", neighbourhood: 'Toronto', address: '895 Fake Street West', price_range: '10-40', menu: 'Lots of drinks.', summary: "Aspiring wizards drink butterbeer here and host mind-numbing after parties", owner_id: nil},
+  {name: "The Babylon Club", neighbourhood: 'North York', address: '9588 North York', price_range: '40-60', menu: 'Pricey food.', summary: "Say hello to your little friend, or other friends at this south Florida staple", owner_id: nil}
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
