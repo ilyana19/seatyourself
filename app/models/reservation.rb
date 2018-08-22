4001655ac273e78b2090dd7901741e6152c436c6
schwarro <rob.ap.schwartz@gmail.com>
2018-08-22 15:13:52 -0400

    completed validation for date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,10 +3,17 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
 
   validate :check_time
+  validate :check_date
 
   def check_time
     if  restaurant.opening_hour >= self.time_slot.hour || restaurant.closing_hour <= self.time_slot.hour
       errors.add(:base, "Reservation must be within operation hours!")
+    end
+  end
+
+  def check_date
+    if Time.now >= self.time_slot
+      errors.add(:base, "Reservations cannot be made in the past!")
     end
   end
 end
