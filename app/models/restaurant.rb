533792caa3f96a078539753664d94c0c045311f0
ilyana19 <aries_2152@hotmail.com>
2018-09-03 13:48:04 -0400

    styled index page restaurant info

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,8 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   belongs_to :user
   # has_many :users, through: :reservations
+
+  def business_hours
+    "#{opening_hour}:00 AM - #{closing_hour-12}:00 PM"
+  end
 end
